7f92779a0bd5d0c9590baf1a8ca1dcc8042f9f76
Marina Chemezova <aniramskaya@gmail.com>
2023-10-14 14:51:49 +0600

    Comments with scenarios

--- Features/RecipeList/Tests/RecipieListTests.swift
+++ Features/RecipeList/Tests/RecipieListTests.swift
@@ -8,6 +8,39 @@
 import XCTest
 @testable import RecipieList
 
+/*
+ Сценарий загрузки списка рецептов
+ 
+ Проверить время последней загрузки данных и убедиться, что оно пустое или прошел час или более.
+ Запросить данные с сервера
+ Запомнить новое время последней загрузки
+ Вернуть список рецептов вызывающему коду
+
+ Расширения
+
+ 1a. Прошло менее часа с момента последней загрузки
+ вернуть имеющиеся в памяти данные
+
+ 2a. Выполнение запроса к серверу завершилось ошибкой и в памяти есть ранее загруженные данные
+ вернуть имеющиеся данные
+
+ 2б. Выполнение запроса к серверу завершилось ошибкой и в памяти нет ранее загруженных данных
+ вернуть ошибку, которая пришла от сервера
+
+ */
+/*
+ Recipe list loading scenario
+ 
+ Check last loaded time and ensure it is empty or an hour or more has passed since
+ Request new data from server
+ Memorize last loaded time
+ Return recipie list to the calling code
+ 
+ 1a. Less then an hour has passed since last load: return in-memory data
+ 2a. Remote loading has failed and there are in-memory data: return in-memory data
+ 2b. Remote loading has failed and there are no in-memory data: return remote loading error
+*/
+
 protocol DTOLoader {
     func load(completion: @escaping (Result<RecipeListDTO, Error>) -> Void)
 }
