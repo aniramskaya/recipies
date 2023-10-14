e52f6ab1ae3407bf21409c726b2a76b9c1ed68f7
Marina Chemezova <aniramskaya@gmail.com>
2023-10-14 14:32:14 +0600

    Пустая библиотека RecipieList

---
+++ Features/RecipeList/Tests/RecipieListTests.swift
@@ -0,0 +1,12 @@
+//
+//  RecipieListTests.swift
+//  RecipieListTests
+//
+//  Created by Марина Чемезова on 14.10.2023.
+//
+
+import XCTest
+@testable import RecipieList
+
+final class RecipieListTests: XCTestCase {
+}
