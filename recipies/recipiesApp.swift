dd0c7b6f66da88db664b23899f0ad9ccbb753305
Marina Chemezova <aniramskaya@gmail.com>
2023-10-14 14:29:45 +0600

    Пустой проект iOS приложения

---
+++ recipies/recipiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipiesApp.swift
+//  recipies
+//
+//  Created by Марина Чемезова on 14.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct recipiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
