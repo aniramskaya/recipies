3cf432de2450943a5b770c85a58ba3b7a8618011
Marina Chemezova <aniramskaya@gmail.com>
2023-10-14 14:45:01 +0600

    Initializer test

--- Features/RecipeList/Tests/RecipieListTests.swift
+++ Features/RecipeList/Tests/RecipieListTests.swift
@@ -12,5 +12,31 @@ protocol DTOLoader {
     func load(completion: @escaping (Result<RecipeListDTO, Error>) -> Void)
 }
 
+class RecipieListLoader {
+    let dtoLoader: DTOLoader
+    
+    init(dtoLoader: DTOLoader) {
+        self.dtoLoader = dtoLoader
+    }
+}
+
 final class RecipieListTests: XCTestCase {
+    func test_init_doesNothing() throws {
+        let spy = DTOLoaderSpy()
+        let sut = RecipieListLoader(dtoLoader: spy)
+        
+        XCTAssertEqual(spy.messages, [])
+    }
+}
+
+class DTOLoaderSpy: DTOLoader {
+    enum Message: Equatable {
+        case load
+    }
+    
+    var messages: [Message] = []
+    
+    func load(completion: @escaping (Result<RecipeListDTO, Error>) -> Void) {
+        messages.append(.load)
+    }
 }
