07f20ec85f9d6741a0048f7357587fbee3fca03f
Marina Chemezova <aniramskaya@gmail.com>
2023-11-06 13:43:52 +0600

    RecipieListExpirationPolicyTests

--- Features/RecipeList/Tests/RecipieListExpirationPolicyTests.swift
+++ Features/RecipeList/Tests/RecipieListExpirationPolicyTests.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import XCTest
+@testable import RecipieList
+
+/*
+ ✅ Если Data() - timestamp < expirationTimeout вернуть true
+ ✅ Если Data() - timestamp == expirationTimeout вернуть false
+ ✅ Если Data() - timestamp > expirationTimeout вернуть false
+ */
+/*
+ If Data() - timestamp < expirationTimeout return true
+ If Data() - timestamp == expirationTimeout return false
+ If Data() - timestamp > expirationTimeout return false
+ */
+
+struct RecipieListExpirationPolicy: TimestampExpirationPolicy {
+    let timeout: TimeInterval
+    init(timeout: TimeInterval) {
+        self.timeout = timeout
+    }
+    
+    func isValid(_ timestamp: Date) -> Bool {
+        Date().timeIntervalSince(timestamp) < timeout
+    }
+}
+
+class RecipieListExpirationPolicyTests: XCTestCase {
+    func test_isValid_returnsTrueIfTimestampIsLessThanExpirationTime() throws {
+        let sut = RecipieListExpirationPolicy(timeout: 10)
+        
+        XCTAssertTrue(sut.isValid(Date() - 9))
+    }
+    
+    func test_isValid_returnsFalseIfTimestampIsEqualToExpirationTime() throws {
+        let sut = RecipieListExpirationPolicy(timeout: 10)
+        
+        XCTAssertFalse(sut.isValid(Date() - 10))
+    }
+    
+    func test_isValid_returnsFalseIfTimestampIsGreaterThanExpirationTime() throws {
+        let sut = RecipieListExpirationPolicy(timeout: 10)
+        
+        XCTAssertFalse(sut.isValid(Date() - 11))
+    }
+}
