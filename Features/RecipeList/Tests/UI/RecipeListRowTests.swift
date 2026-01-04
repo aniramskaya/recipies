d63e4f5652e080c638349da8c13c87f5bc8ed2da
Marina Chemezova <aniramskaya@gmail.com>
2026-01-04 13:26:15 +0500

    Тестирование контента компонента с протечкой деталей реализации в тест

--- Features/RecipeList/Tests/UI/RecipeListRowTests.swift
+++ Features/RecipeList/Tests/UI/RecipeListRowTests.swift
@@ -9,6 +9,10 @@ import Foundation
 import Testing
 import SnapshotTesting
 import RecipeUIKit
+import ViewInspector
+@testable import RecipieList
+
+typealias A11y = RecipeListRowA11y
 
 struct RecipeListRowTests {
     @MainActor
@@ -26,4 +30,30 @@ struct RecipeListRowTests {
         assertSnapshot(of: view, as: .image(precision: 0.99, layout: .fixed(width: 375, height: 270)))
     }
 
+    @MainActor
+    @Test("RecipeListRow правильно отображает контент")
+    func RecipeListRowContent() async throws {
+        let sut = RecipeListRow(
+            model: .init(
+                id: UUID(uuidString: "b0a1a334-09b3-4e61-87bf-0b05745388cf")!,
+                name: "Котлета по-киевски",
+                imageSource: .uiImage(.make(withColor: .red)),
+                cookingTimeMins: 35,
+                rating: 4.6
+            )
+        )
+        
+        let inspectable = try sut.inspect()
+        
+        let name = try inspectable.find(viewWithAccessibilityIdentifier: A11y.name).text().string()
+        #expect(name == "Котлета по-киевски")
+
+        #expect(throws: Never.self) {
+            let _ = try inspectable.find(viewWithAccessibilityIdentifier: A11y.rating).find(text: "4.6")
+        }
+        
+        #expect(throws: Never.self) {
+            let _ = try inspectable.find(viewWithAccessibilityIdentifier: A11y.cookingTime).find(text: "35 min")
+        }
+    }
 }
