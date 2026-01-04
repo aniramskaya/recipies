a7aad10980a2516ec04fd1b1d0c9d48b4d58d361
Marina Chemezova <aniramskaya@gmail.com>
2026-01-04 13:57:37 +0500

    Добавлен sourceLocation для того чтобы причина падения теста внутри DSL отображалась в тексте теста

--- Features/RecipeList/Tests/UI/RecipeListRow+DSL.swift
+++ Features/RecipeList/Tests/UI/RecipeListRow+DSL.swift
@@ -10,17 +10,17 @@ import ViewInspector
 @testable import RecipieList
 
 extension RecipeListRow {
-    func assertIsDisplaying(name: String, rating: String, cookingTime: String) throws {
+    func assertIsDisplaying(name: String, rating: String, cookingTime: String, sourceLocation: SourceLocation = #_sourceLocation) throws {
         let inspectable = try self.inspect()
         
         let nameFound = try inspectable.find(viewWithAccessibilityIdentifier: A11y.name).text().string()
-        #expect(nameFound == name)
+        #expect(nameFound == name, "RecipeListRow name expected to be \(name), found \(nameFound) instead", sourceLocation: sourceLocation)
 
-        #expect(throws: Never.self) {
+        #expect(throws: Never.self, "RecipeListRow does not have a rating view with text \(rating)", sourceLocation: sourceLocation) {
             let _ = try inspectable.find(viewWithAccessibilityIdentifier: A11y.rating).find(text: rating)
         }
         
-        #expect(throws: Never.self) {
+        #expect(throws: Never.self, "RecipeListRow does not have a cookingTime view with text \(cookingTime)", sourceLocation: sourceLocation) {
             let _ = try inspectable.find(viewWithAccessibilityIdentifier: A11y.cookingTime).find(text: cookingTime)
         }
     }
